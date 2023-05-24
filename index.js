9e0a58876635cc3f094324945e8e938c22348fc1
chetanguptaa <chetangupta123raj@gmail.com>
2023-05-24 17:49:40 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const {register, login} = require('./controllers/authController');
 const Router = require('./routes/userRoutes');
-
+const connectingToTheDatabase = require('./database/database');
 const app = express();
 const port = 8000;
+
+// connectingToTheDatabase('')
 
 app.use(express.json());
 
