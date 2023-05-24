8ed690cdc7e41bee7dc7a27e68df27312f6ec794
chetanguptaa <chetangupta123raj@gmail.com>
2023-05-24 20:27:00 +0530

    updated authController.js for testing purposes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,51 +1,101 @@
 const bcrypt = require('bcryptjs');
-const User = require('../database/models/userModel');
+const USERS = require('../database/models/userModel');
 
 const register = async (req, res) => {
+    /* -------- DATABASE IMPLEMENTATION FOR LATER-------- */
+    // try {
+    //     const {name, email, password} = req.body;
+    //     const existingUser = await User.findOne({email});
+    //     if(Users.find((x) => x.email = email)) {
+    //         return res.status(409).json({error: 'Email already registered'});
+    //     }
+    //     if(existingUser) {
+    //         return res.status(409).json({error: 'Email already registered'});
+    //     }
+    //     const cryptPassword = await bcrypt.hash(password, 10);
+    //     const newUser = new User({
+    //         name,
+    //         email,
+    //         password: cryptPassword,
+    //     })
+
+    //     await newUser.save();
+    //     res.status(201).json({message: 'Registration Successfull'});
+        
+    // } catch (error) {
+    //     console.log('error registring User: '+ error);
+    //     res.status(500).json({
+    //         error: 'internal server error'
+    //     })
+    // }
     try {
         const {name, email, password} = req.body;
-        const existingUser = await User.findOne({email});
-        if(existingUser) {
-            return res.status(409).json({error: 'Email already registered'});
-        }
+        if(USERS.find((x) => x.email === email)) {
+            return res.status(409).json({
+                error: 'Email already registered'
+            })
+        } 
         const cryptPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            name,
-            email,
-            password: cryptPassword,
+        USERS.push({
+            name, 
+            email, 
+            cryptPassword
         })
-
-        await newUser.save();
-        res.status(201).json({message: 'Registration Successfull'});
-        
-    } catch (error) {
-        console.log('error registring User: '+ error);
+        res.status(201).json({
+            message: 'Registration Successfull'
+        })
+    } catch ( error ) {
+        console.log('error registering user: ' + error);
         res.status(500).json({
-            error: 'internal server error'
+            error: 'Internal server error'
         })
-    }
+    } 
 }
 
 const login = async (req, res) => {
+    /* ----------- DATABASE IMPLEMENTATION FOR LATER ---------- */
+    // try {
+    //     const {email, password} = req.body;
+    //     const user = await User.findOne({email});
+    //     if(!user) {
+    //         return res.status(401).json({
+    //             error: 'Invalid Email or Password'
+    //         })
+    //     }
+    //     const passwordMatch = await bcrypt.compare(password, user.password);
+    //     if(!passwordMatch) {
+    //         return res.status(401).json({
+    //             error: 'Invalid Email or Password'
+    //         })
+    //     }
+    //     res.json({
+    //         message: 'Login Successful'
+    //     })
+    // } catch (error) {
+    //     console.log('error logging in: ' + error);
+    //     res.status(500).json({
+    //         error: 'internal server error'
+    //     })
+    // }
     try {
         const {email, password} = req.body;
-        const user = await User.findOne({email});
+        const user = USERS.find( (x) => x.email === email );
         if(!user) {
             return res.status(401).json({
-                error: 'Invalid Email or Password'
+                error: 'Invalid Email or password'
             })
         }
-        const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = bcrypt.compare(password, user.password);
         if(!passwordMatch) {
             return res.status(401).json({
-                error: 'Invalid Email or Password'
+                error: 'Invalid Email or password'
             })
         }
         res.json({
-            message: 'Login Successful'
+            message: 'Login successfull'
         })
-    } catch (error) {
-        console.log('error logging in: ' + error);
+    } catch( error ) {
+        console.log('error logging in: ' + error)
         res.status(500).json({
             error: 'internal server error'
         })
