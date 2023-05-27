0885650af5a97534c040cf08503a38d12e9ca00d
chetanguptaa <chetangupta123raj@gmail.com>
2023-05-27 18:48:55 +0530

    updated userModel.js

--- database/models/userModel.js
+++ database/models/userModel.js
@@ -1,20 +1,20 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//     }
-// })
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    }
+})
 
-// const User = mongoose.model('User', userSchema);
+const USERS = mongoose.model('User', userSchema);
 
-// module.exports = USERS;
+module.exports = USERS;
